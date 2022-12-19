5f543ac10266822cd56757ce6f9453d03cc9af28
praveen <praveenkk8693@gmail.com>
2022-12-19 07:21:21 +0530

    adding report plugins

--- src/test/java/com/asc/test/runner/TestRunner.java
+++ src/test/java/com/asc/test/runner/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="D:\\projects\\com-asc-jenkins-tesing\\src\\test\\resources\\features",glue= {"com.asc.test.stepdefs"})
+@CucumberOptions(features="D:\\projects\\com-asc-jenkins-tesing\\src\\test\\resources\\features",
+glue= {"com.asc.test.stepdefs"},
+plugin={"html:target/test-results.html","json:target/test-results.json","junit:target/test-results.xml"})
 public class TestRunner {
 
 }
